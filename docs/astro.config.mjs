e3ef4dba8b6422c81f4157aa18637db3268252cf
Ozzie Neher <oz.neher@gmail.com>
2025-06-08 20:26:38 +1000

    update docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   redirects: {
     "/": {
-      destination: "/getting-started/why-viper",
+      destination: "/getting-started/overview",
       status: 301,
     },
   },
