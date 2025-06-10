11dad150c78a7236168fcd870d74377bfd747013
Ozzie Neher <oz.neher@gmail.com>
2025-06-10 14:07:01 +1000

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/ozmos-dev/viper",
         },
       ],
       sidebar: [
